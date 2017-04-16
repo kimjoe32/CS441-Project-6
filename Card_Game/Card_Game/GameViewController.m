8e2fcafd08a2c13808b94bcba7c0a92f4afad337
ETS Admin <etsadmin@ETSMacbook1.local>
2017-04-16 18:41:31 -0400

    test cases

--- Card_Game/Card_Game/GameViewController.m
+++ Card_Game/Card_Game/GameViewController.m
@@ -28,8 +28,10 @@
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
     
-    Hand * h1 = [[Hand alloc] init:@"8C" card2:@"TS" card3:@"KC" card4:@"9H" card5:@"4S"];
-    Hand * h2 = [[Hand alloc] init:@"7D" card2:@"2S" card3:@"5D" card4:@"3S" card5:@"AC"];
+    Hand * h1 = [[Hand alloc] init];
+    [h1 addCards:@"8C" card2:@"TS" card3:@"KC" card4:@"9H" card5:@"4S"];
+    Hand * h2 = [[Hand alloc] init];
+    [h2 addCards:@"7D" card2:@"2S" card3:@"5D" card4:@"3S" card5:@"AC"];
     
     NSInteger winner = [h1 checkWinnerAgainst:h2];
     NSLog(@"%ld", (long)winner);
